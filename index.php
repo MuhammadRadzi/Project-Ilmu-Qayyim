88b147c024b0ef7e2eec1daceaeadc8576d9c067
Mimissiee <growfitzgt@gmail.com>
2025-08-29 14:51:01 +0800

    Pull

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
             <li><a href="subjects.html">Subjects</a></li>
             <li><a href="contact.html">Contact</a></li>
             <li><a href="about.html">About</a></li>
-            </li><a href="login.php" class="login-button">Login</a></li>
+            <li><a href="login.php" class="login-button">Login</a></li>
         </ul>
         
     </nav>
